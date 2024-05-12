fcac31c7ce5755b7e874698bd5138970a114eadc
Tharanya <tharanyasugumaran@gamil.com>
2024-05-12 17:55:31 +0530

    Graph Traversal

--- Graph/Graph_DFS_Iterative.js
+++ Graph/Graph_DFS_Iterative.js
@@ -12,22 +12,22 @@ class Graph {
     this.adjacencyList[v2].push(v1);
   }
 
-  depthFirstSearch(start) {
-    let result = []; //this will return the path
-    let visited = {}; //this will show if the vertex has been visited or not
-    let adjacencyList = this.adjacencyList;
-
-    (function dfs(vertex) {
-      if (!vertex) return null;
-      visited[vertex] = true;
-      result.push(vertex);
-      adjacencyList[vertex].forEach((neighbor) => {
+  depthFirstSearchIterative(start) {
+    let result = [];
+    let visited = {};
+    let stack = [start]; //push start into stack initally
+    visited[start] = true; //make start as visited
+    let currentVertex;
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
         if (!visited[neighbor]) {
-          return dfs(neighbor);
+          visited[neighbor] = true;
+          stack.push(neighbor);
         }
       });
-    })(start); //initialize the function with start without calling and we can't use this keyword
-
+    }
     return result;
   }
 }
@@ -46,7 +46,7 @@ myGraph.addEdge("C", "E");
 myGraph.addEdge("D", "E");
 myGraph.addEdge("D", "F");
 myGraph.addEdge("E", "F");
-console.log(myGraph.depthFirstSearch("A")); //[ 'A', 'B', 'D', 'E', 'C', 'F' ]
+console.log(myGraph.depthFirstSearchIterative("A")); //[ 'A', 'C', 'E', 'F', 'D', 'B' ]
 
 //             A
 //            /  \
