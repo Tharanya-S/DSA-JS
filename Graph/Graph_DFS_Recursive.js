f2e98ac01dc140f46320fdfd00f4b6eafc8d765e
Tharanya <tharanyasugumaran@gamil.com>
2024-05-12 14:09:12 +0530

    Graph Traversal Recursively

---
+++ Graph/Graph_DFS_Recursive.js
@@ -0,0 +1,57 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  depthFirstSearch(start) {
+    let result = []; //this will return the path
+    let visited = {}; //this will show if the vertex has been visited or not
+    let adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start); //initialize the function with start without calling and we can't use this keyword
+
+    return result;
+  }
+}
+
+const myGraph = new Graph();
+myGraph.addVertex("A");
+myGraph.addVertex("B");
+myGraph.addVertex("C");
+myGraph.addVertex("D");
+myGraph.addVertex("E");
+myGraph.addVertex("F");
+myGraph.addEdge("A", "B");
+myGraph.addEdge("A", "C");
+myGraph.addEdge("D", "B");
+myGraph.addEdge("C", "E");
+myGraph.addEdge("D", "E");
+myGraph.addEdge("D", "F");
+myGraph.addEdge("E", "F");
+console.log(myGraph.depthFirstSearch("A"));
+
+//             A
+//            /  \
+//           B    C
+//           |    |
+//           D----E
+//            \  /
+//              F
